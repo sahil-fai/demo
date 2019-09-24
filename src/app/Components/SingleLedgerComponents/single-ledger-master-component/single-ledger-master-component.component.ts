1b337621072ecc40813bb01eb08ddb6b3299e401
utsah_paikray <utsah@hashchainft.com>
2019-09-24 10:24:24 +0530

    bugs fixed

--- src/app/Components/SingleLedgerComponents/single-ledger-master-component/single-ledger-master-component.component.ts
+++ src/app/Components/SingleLedgerComponents/single-ledger-master-component/single-ledger-master-component.component.ts
@@ -1,6 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { HelperService } from '../../../services/helper-service/helper.service';
-import { BusinessService } from '../../../services/business-service/business.service';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
+import {
+  HelperService
+} from '../../../services/helper-service/helper.service';
+import {
+  BusinessService
+} from '../../../services/business-service/business.service';
 
 @Component({
   selector: 'app-single-ledger-master-component',
@@ -10,14 +17,20 @@ import { BusinessService } from '../../../services/business-service/business.ser
 export class SingleLedgerMasterComponentComponent implements OnInit {
   CurrentCompanyName: any;
   public isNavOpen = false;
+  businessList: any;
 
-  constructor(private helper: HelperService, public BusinessService: BusinessService) { }
+  constructor(private helper: HelperService, public BusinessService: BusinessService) {}
 
   ngOnInit() {
     const companyid = Number(this.helper.getcompanyId());
+    const userId = Number(this.helper.getuserId());
     this.BusinessService.getCompanyInformation(companyid).subscribe(res => {
-    this.CurrentCompanyName = res.legalname;
-   });
+      this.CurrentCompanyName = res.legalname;
+    });
+    this.BusinessService.getListOfbusinesses(userId).subscribe(res => {
+      console.log(res);
+      this.businessList = res;
+    });
   }
   public closeNav() {
     this.isNavOpen = !this.isNavOpen;
@@ -25,6 +38,20 @@ export class SingleLedgerMasterComponentComponent implements OnInit {
   public openNav() {
     this.isNavOpen = !this.isNavOpen;
   }
+  // public viewBusiness(businessID) {
+  //   this.BusinessService.view(businessID).subscribe(res => {
+  //     let token = localStorage.getItem('business_token');
+  //    // this.helper.business.remove();
+  //     //this.helper.business.set(res);
+  //     localStorage.setItem('business_token', token);
+  //   //  this.setCurrency();
+  //     this._switchCompany.switchCompany();
+
+  //   });
+  //   this.isModal = false;
+  //   setTimeout(() => {
+  //     this.isModal = true;
+  //   }, 500);
+  // }
 
 }
-
--- src/app/services/business-service/business.service.ts
+++ src/app/services/business-service/business.service.ts
@@ -20,8 +20,6 @@ export class BusinessService {
   }
 
   getAllVendors(id: number): Observable<any> {
-    //console.log(id);
-    // tslint:disable-next-line: max-line-length
     return this.http.get<any>('business/vendors' + '?filter={"where":{"referencecompanyid":' + id + '},"limit":100}', {
    });
   }
