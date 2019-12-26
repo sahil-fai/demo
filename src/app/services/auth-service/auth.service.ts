b7d6afd32583aee463055c446b2fbcca3901010d
sahil <sahil@hashchainft.com>
2019-12-26 12:11:43 +0530

    issue resolved

--- src/app/services/auth-service/auth.service.ts
+++ src/app/services/auth-service/auth.service.ts
@@ -11,17 +11,28 @@ export class AuthService {
 
   enroll(data): Observable<any> {
     const env = environment;
-    return this.http.post<any>('users', {
-      email: data.username,
-      lastname: data.lastName,
-      password: data.password,
-      firstName: data.firstName,
-      roleType:data.role,
-      inviteby:data.inviteby,
-      invitetype:data.invitetype,
-      invitecompanyid:data.invitecompanyid,
-      inviteuserid:data.inviteuserid      
-      });
+
+  let signupdetails = {
+    email: data.username,
+    lastname: data.lastName,
+    password: data.password,
+    firstName: data.firstName,
+    roleType:data.role,
+    inviteby:data.inviteby,
+    invitetype:data.invitetype,
+    invitecompanyid:data.invitecompanyid,
+    inviteuserid:data.inviteuserid      
+    };
+  
+    if (signupdetails.invitecompanyid == null || signupdetails.invitecompanyid == undefined)
+    {
+      delete signupdetails.invitecompanyid;
+      delete signupdetails.invitetype;
+      delete signupdetails.inviteuserid;
+      delete signupdetails.inviteby;
+    }
+
+    return this.http.post<any>('users', signupdetails);
   }
 
   login(data): Observable<any> {
