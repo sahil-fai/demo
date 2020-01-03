4f5c8324c18f9f32c8db97d6235ac9d9e0cbc21b
sahil <sahil@hashchainft.com>
2020-01-03 10:57:59 +0530

    ui bugs resolved

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  host: 'https://beta.singleledger.org/',
+  host: 'https://localhost:3000/',
   production: false
 };
