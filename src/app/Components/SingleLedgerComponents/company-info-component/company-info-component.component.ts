8a3e6feb20adc4bbd0b24e01888e48dedd750b32
pankaj cheema <pankaj.cheema@hashchainft.com>
2020-01-30 15:10:17 +0530

    SIN-904, SIN-911 fixed by brij

--- src/app/Components/SingleLedgerComponents/company-info-component/company-info-component.component.ts
+++ src/app/Components/SingleLedgerComponents/company-info-component/company-info-component.component.ts
@@ -25,11 +25,10 @@ export class CompanyInfoComponentComponent implements OnInit , OnDestroy {
     const companyid = Number(this.helper.getcompanyId());
     const filter = '?filter={"include":[{"relation":"all"}]}';
     this.businessService.getCompanyInformation(companyid, filter).subscribe(res => {
-      let compinfo = this.helper.convertJsonKeysToLower(res);
-      if(compinfo.currency[0].currencyname) {
+      let compinfo = this.helper.convertJsonKeysToLower(res); 
+      if(compinfo && compinfo.currency[0] && compinfo.currency[0].currencyname) {
           localStorage.setItem('CompanyCurrency', compinfo.currency[0].currencyname);
       }
-      console.log(compinfo);
       this.companyinfo = compinfo;
       this.helper.setplatformId(compinfo.platformid);
     });
--- src/app/Dashboard/single-ledger-business-list/single-ledger-business-list.component.ts
+++ src/app/Dashboard/single-ledger-business-list/single-ledger-business-list.component.ts
@@ -1,26 +1,9 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
-import {
-  BusinessService
-} from '../../services/business-service/business.service';
-import {
-  from
-} from 'rxjs';
-
-import {
-  HelperService
-} from '../../services/helper-service/helper.service';
-import {
-  Router
-} from '@angular/router';
-import {
-  SwitchCompanyService
-} from '../../services/switch-company-service/switch-company.service';
+import { Component, OnInit } from '@angular/core';
+import { BusinessService } from '../../services/business-service/business.service';
+import { HelperService } from '../../services/helper-service/helper.service';
+import { Router } from '@angular/router';
+import { SwitchCompanyService } from '../../services/switch-company-service/switch-company.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { SocketService } from 'src/app/services/socket.service';
-import { ToastrService } from 'ngx-toastr';
 import { IBusinessModel } from '../../Interface/business/business-model.interface';
 
 @Component({
@@ -29,16 +12,12 @@ import { IBusinessModel } from '../../Interface/business/business-model.interfac
   styleUrls: ['./single-ledger-business-list.component.less']
 })
 export class SingleLedgerBusinessListComponent implements OnInit {
-  // CompanyName: string;
-  // keywords: string;
+
   title = 'Business(s) List';
-  public businessListActual: IBusinessModel[] = [];
-  public businessListFiltered: IBusinessModel[] = [];
-  companylist: Array < any > ;
   totalRec: number;
-  page = 1;
-  length: number;
-  public isBusinessLoaded: boolean;
+  public companylist: IBusinessModel[];
+  public businessListActual: IBusinessModel[] = [];
+  public isBusinessLoaded: boolean = false;
   public maxSize = 7;
   public directionLinks = true;
   public autoHide = true;
@@ -51,88 +30,65 @@ export class SingleLedgerBusinessListComponent implements OnInit {
     screenReaderPageLabel: 'page',
     screenReaderCurrentLabel: `You're on page`
   };
-
   public config = {
     itemsPerPage: this.selectedValue,
     currentPage: 1,
     totalItems: this.totalRec,
     id: 'companylist'
   };
-
   public pageNumber = 1;
   public pageSizeOptions: number[] = [5, 15, 25];
   public numberOfpages: number[];
-  businesslsist: any;
   switchCompanySubscription: any;
   submitted: boolean;
-  // businessListFiltered: any;
-  // actualBusinessList: any[];
-  constructor(public businessService: BusinessService,
-              private helper: HelperService, private router: Router,
-              private switchCompany: SwitchCompanyService,
-              private _fb: FormBuilder
-              ) {
-    this.switchCompanySubscription = this.switchCompany.companySwitched.subscribe(
-      () => {
-        this.ngOnInit();
-      }
-    );
-  }
   formSearch: FormGroup;
-  ngOnInit() {
-
-    const userrid = Number(this.helper.getuserId());
-    this.businessService.getListOfbusinesses(userrid).subscribe(res => {
-      if (res.length >= 0) {
-        this.companylist = this.businessListFiltered = res;
-        this.businessListActual =  res;
-        this.totalRec = this.companylist.length;
-        this.isBusinessLoaded = true;
-      } else {
-        this.companylist = [];
-      }
+  constructor(public businessService: BusinessService, private helper: HelperService, private router: Router, private switchCompany: SwitchCompanyService, private _fb: FormBuilder) {
+    this.switchCompanySubscription = this.switchCompany.companySwitched.subscribe(() => {
+      this.ngOnInit();
     });
+  }
+
+  ngOnInit() {
+    const userrid = Number(this.helper.getuserId());
+    if(userrid) {
+        this.businessService.getListOfbusinesses(userrid).subscribe(res => {
+          if (res && res.length > 0) {
+            this.companylist = res;
+            this.businessListActual = res;
+            this.totalRec = this.companylist.length;
+            this.isBusinessLoaded = true;
+          } else {
+            this.companylist = [];
+            this.totalRec = 0;
+            this.isBusinessLoaded = false;
+          }
+        });
+    }
     this._createForm();
   }
+
   private _createForm() {
-    this.formSearch = this._fb.group({
-      keywords: ['', [Validators.required]]
-    });
+    this.formSearch = this._fb.group({ keywords: ['', [Validators.required]] });
   }
+
   public viewBusiness(companyid) {
     this.helper.setcompanyId(companyid);
     this.router.navigate(['/business', 'company-info']);
   }
-  public openDialog(){}
-  // public onFilter() {
-  //   let self = this;
-  //   this.submitted = true;
-  //   if (this.formSearch.invalid) { return; }
-  //   self.businessListFiltered = [];
-  //   this.businessListActual.forEach(i => {
-  //     if (i.legalName.toLocaleLowerCase().indexOf(self.formSearch.controls['keywords'].value.toLocaleLowerCase()) !== -1) {
-  //       // this.filterBussinessList = true;
-  //       this.businessListFiltered.push(i);
-  //     } else {
-  //         // this.filterBussinessList = true;
-  //     }
-  //   });
 
-  // }
+  public openDialog() { }
+
   public onFilter() {
     this.submitted = true;
-
     if (this.formSearch.invalid) { return; }
-    this.businessListFiltered = [];
     this.companylist = [];
-    this.isBusinessLoaded = false;
     this.businessListActual.forEach(i => {
       if (i['legalName'].toLocaleLowerCase().indexOf(this.formSearch.controls['keywords'].value.toLocaleLowerCase()) !== -1) {
-        this.isBusinessLoaded = true;
-       this.companylist = [i];
+        this.companylist = [i];
       }
     });
   }
+
   public onReset() {
     this.pageNumber = 0;
     this.isBusinessLoaded = true;
